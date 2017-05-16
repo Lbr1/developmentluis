b5832205c90e4ff952de3d16bfa98e5f3d8ae35b
Lbr1 <lbrbrito@gmail.com>
2017-05-16 14:29:59 +0100

    v2

--- src/main/java/pt/uminho/sisbi/twetter/App.java
+++ src/main/java/pt/uminho/sisbi/twetter/App.java
@@ -1,4 +1,11 @@
 package pt.uminho.sisbi.twetter;
+import java.io.IOException;
+import java.util.*;
+
+import pt.uminho.sisbi.twetter.api.TweetAPI;
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.*;
 
 /**
  * Hello world!
@@ -6,8 +13,163 @@ package pt.uminho.sisbi.twetter;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws TwitterException, IOException
     {
+    	
+    	//TweetAPI api = new TweetAPI();
+    	
         System.out.println( "Hello World!" );
+        
+        String consumerKey = "9Q0DpYL9vJtZcuxNinsA1RjWn";
+        
+        //Consumer Secret
+        String consumerSecret = "qohM464rRWVYXcNPmrOI7qhkqO1HJHjtGDXkxl0MsLTMdr7AAw";
+ 
+        //Access Token
+        String accessToken = "2841171466-dL9I79GWTG6QKEZW5Swy3e455EXermjkDGycTng";
+ 
+        //Access Token Secret
+        String accessTokenSecret = "tccuixlMuOK7r4H2Eu7jWVa5MDlp9FOohqkX3nbjSNU8N";
+        
+      //Instantiate a re-usable and thread-safe factory
+        TwitterFactory twitterFactory = new TwitterFactory();
+      //Instantiate a new Twitter instance
+        Twitter twitter = twitterFactory.getInstance();
+      //setup OAuth Consumer Credentials
+        twitter.setOAuthConsumer(consumerKey, consumerSecret);
+      //setup OAuth Access Token
+        twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+        
+        /*System.out.println("O ScreenName do Twitter "+ twitter.getScreenName());
+        System.out.println("Os Tweets do Twitter "+ twitter.tweets());
+        System.out.println("Os Friends do Twiter "+ twitter.getFriendsIDs(1));
+        System.out.println("ToString do Twiter "+ twitter.toString());
+        System.out.println("Os Friends do Twiter "+ twitter.friendsFollowers());
+        System.out.println("Account Settings "+ twitter.getAccountSettings());
+        System.out.println("Languages "+ twitter.getLanguages());
+        
+        */
+        
+          
+       //*****************get the recents tweets of my account*************
+       /* 
+        List<Status> statuses = TweetAPI.newTweets(twitter);
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName()+ " -> " +
+                               status.getText());
+            String urls= "https://twitter.com/" + status.getUser().getScreenName() 
+            	    + "/status/" + status.getId();
+            System.out.println("Post Url -> "+urls);
+            System.out.println("Status getLocation -> "+status.getUser().getLocation());
+            System.out.println("Number of Retweets -> "+status.getRetweetCount());
+            System.out.println("Status getURL -> "+status.getUser().getURL());
+            System.out.println("\n");
+            System.out.println("\n");
+            
+        }
+        */
+        //************Send a message for a specific user***************
+        //TweetAPI.SendMessage(twitter, "Olá Luís 222", "@asdasdafsw");
+        
+        System.out.println("DONE ");
+        /*
+        Query query = new Query("java");  
+        QueryResult result = twitter.search(query);
+        System.out.println("OLHA OS TWEETS : " + result.getCount());
+        System.out.println("OLHA OS TWEETS : " + result.getMaxId());
+        System.out.println("retweet "+ twitter.toString());
+		*/
+        
+        
+        //tratamento do ficheiro
+        /*TweetAPI.ficheiro(twitter, "teste", "MotoGP");
+        List<Status> statuses2 = TweetAPI.tweetsUtilizador(twitter, "@MotoGP");
+        System.out.println("OLHA OS TWEETS2: " + statuses2.toString());
+        System.out.println("OLHA OS TWEETS3: " + statuses2.size());
+ */
+        
+        
+        //*******************Create a Tweet**********************
+         
+     // TweetAPI.updateStatus(twitter);
+        
+      
+      //*******************Search Querys**********************
+        
+        TweetAPI.searchQuery(twitter);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       //ir buscar x posts sem serem repetidos
+
+       /* ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setOAuthConsumerKey("");
+        cb.setOAuthConsumerSecret("");
+        cb.setOAuthAccessToken("");
+        cb.setOAuthAccessTokenSecret("");
+
+        Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+
+        int pageno = 1;
+        String user = "cnn";
+        List statuses = new ArrayList();
+
+        while (true) {
+
+          try {
+
+            int size = statuses.size(); 
+            Paging page = new Paging(pageno++, 100);
+            statuses.addAll(twitter.getUserTimeline(user, page));
+            if (statuses.size() == size)
+              break;
+          }
+          catch(TwitterException e) {
+
+            e.printStackTrace();
+          }
+        }
+
+        System.out.println("Total: "+statuses.size());
     }
+    -------------------------------------------------------
+    Query query = new Query("test");
+	query.setCount(100);
+	
+	int searchResultCount;
+	long lowestTweetId = Long.MAX_VALUE;
+	
+	do {
+	    QueryResult queryResult = twitterInstance.search(query);
+	
+	    searchResultCount = queryResult.getTweets().size();
+	
+	    for (Status tweet : queryResult.getTweets()) {
+	
+	        // do whatever with the tweet
+	
+	        if (tweet.getId() < lowestTweetId) {
+	            lowestTweetId = tweet.getId();
+	            query.setMaxId(lowestTweetId);
+	        }
+	    }
+	
+	} while (searchResultCount != 0 && searchResultCount % 100 == 0);
+	    */
+}
+    
+    
 }
---
+++ src/main/java/pt/uminho/sisbi/twetter/api/TweetAPI.java
@@ -0,0 +1,107 @@
+package pt.uminho.sisbi.twetter.api;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import twitter4j.DirectMessage;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class TweetAPI {
+
+	public static List<Status> newTweets(Twitter twitter) throws TwitterException {   
+        List<Status> statuses = twitter.getHomeTimeline();  
+        return statuses;  
+    }
+	
+	public static Status updateStatus(Twitter twitter) throws TwitterException, IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Insira o seu Tweet: \n");
+			String status = br.readLine();
+			System.out.println("Successfully updated the status to [" + status + "].");
+	      return twitter.updateStatus(status);  
+	}
+	
+	public static DirectMessage SendMessage(Twitter twitter, String mensagem, String destinatarioID)  
+	         throws TwitterException {
+	      return twitter.sendDirectMessage(destinatarioID, mensagem);  
+	}  
+	
+	public static void searchQuery(Twitter twitter) throws IOException{
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Insira a sua Query: \n");
+		//String args[] = br.readLine();
+		String args = "motoGP";
+		if(args.length() < 1){
+			System.out.println("java twitter4j.examples.search.SearchTweets [query]");
+            System.exit(-1);
+		}
+		try {
+			Query query = new Query(args);
+            QueryResult result;
+            do {
+                result = twitter.search(query);
+                List<Status> tweets = result.getTweets();
+                for (Status tweet : tweets) {
+                    System.out.println("@" + tweet.getUser().getScreenName() + " -> " + tweet.getText());
+                }
+            } while ((query = result.nextQuery()) != null);
+            System.exit(0);
+			
+		} catch (TwitterException te) {
+			te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+		}
+		
+	}
+	
+	public static List<Status> tweetsUtilizador(Twitter twitter, String twitterID)  
+	         throws TwitterException {    
+	      List<Status> statuses = twitter.getUserTimeline(twitterID);  
+	      return statuses;  
+	   }  
+	
+	public static  void ficheiro(Twitter twitter, String fileName,String queryS)  {
+	         
+	try {	
+		int i=0;
+		String path = "C:" + File.separator + "Users" + File.separator + "Brito"+ File.separator + "Desktop" 
+		+ File.separator + "silicoTwitter"+ File.separator + "twitter"+ File.separator + "texte.txt";
+		// Use relative path for Unix systems
+		File f = new File(path);
+
+		f.getParentFile().mkdirs(); 
+		f.createNewFile();
+		
+		//File file = new File("twitter/testes/"+ fileName+".txt");
+		FileWriter fw = new FileWriter(f, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		QueryResult result;
+		Query query = new Query(queryS);
+		do {
+		i++;
+		result = twitter.search(query);
+		List<Status> tweets = result.getTweets();
+		for (Status tweet : tweets) {
+		System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText() + "\n");
+		bw.write("@" + tweet.getUser().getScreenName() + " - " + tweet.getText() +"\n\n\n\n\n\n");
+		}
+		}
+		while (i<=10/*(query = result.nextQuery()) != null*/);
+		System.exit(0);
+		bw.close();
+		} catch (TwitterException te) {
+		te.printStackTrace();
+		System.out.println("Failed to search tweets: " + te.getMessage());
+		System.exit(-1);
+		} catch (IOException e) {    e.printStackTrace();    }}
+}
